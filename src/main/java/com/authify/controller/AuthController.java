d24d1cea532d7ef0755112df0675179d981aee62
AMITFARSWAN21 <amitfarswan2104@gmail.com>
2025-06-21 12:32:26 +0530

    Added logout functionality

--- src/main/java/com/authify/controller/AuthController.java
+++ src/main/java/com/authify/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.authify.io.ResetPasswordRequest;
 import com.authify.service.AppUserDetailsService;
 import com.authify.service.ProfileService;
 import com.authify.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -141,6 +142,22 @@ public class AuthController {
            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,e.getMessage());
         }
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0) // expire the cookie immediately
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body("Logout successful");
+    }
+
 
 
 
